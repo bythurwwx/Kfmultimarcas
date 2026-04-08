7562348e60f1bf576aca6c1568f52d6d47d704c1
bythurwwx <arthr338.mmz@gmail.com>
2026-04-08 16:19:50 -0300

    Criar questions.js

---
+++ questions.js
@@ -0,0 +1,12 @@
+const challenges = [
+    {
+        question: "Como se diz 'Bom dia' em francês?",
+        options: ["Bonsoir", "Bonjour", "Merci", "Salut"],
+        answer: "Bonjour"
+    },
+    {
+        question: "Complete: 'Je ___ étudiant.'",
+        options: ["es", "est", "suis", "sommes"],
+        answer: "suis"
+    }
+];
